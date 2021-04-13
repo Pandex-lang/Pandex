708f4889253a51c4363952c3ba0b83f98ca85060
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-04-13 14:16:03 +0430

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -1,4 +1,5 @@
 # install requirements for debian based linux distroes
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install gcc
+sudo su
+apt-get update
+apt-get upgrade
+apt-get install gcc
