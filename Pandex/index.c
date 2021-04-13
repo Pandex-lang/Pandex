23bc26dce2fcf0897d4c32aaeb8d97717681d368
Ashkan Laei <50172698+AshkanLaei@users.noreply.github.com>
2021-04-13 13:41:27 +0430

    Better numToAlph function

--- Pandex/index.c
+++ Pandex/index.c
@@ -19,35 +19,8 @@ int whereIs(char *string , char where){
     }
 }
 
-char numToAlph(int num){
-    switch(num){
-        case 1:return 'a';
-        case 2:return 'b';
-        case 3:return 'c';
-        case 4:return 'd';
-        case 5:return 'e';
-        case 6:return 'f';
-        case 7:return 'g';
-        case 8:return 'h';
-        case 9:return 'i';
-        case 10:return 'j';
-        case 11:return 'k';
-        case 12:return 'l';
-        case 13:return 'm';
-        case 14:return 'n';
-        case 15:return 'o';
-        case 16:return 'p';
-        case 17:return 'q';
-        case 18:return 'r';
-        case 19:return 's';
-        case 20:return 't';
-        case 21:return 'u';
-        case 22:return 'v';
-        case 23:return 'w';
-        case 24:return 'x';
-        case 25:return 'y';
-        case 26:return 'z';
-    }
+char numToAlph(int num) {
+    return (num >= 1 && num <= 26) ? (char)(96 + num) : ' ';
 }
 
 int isBe(char *str , char delim){
